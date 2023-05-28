bdeea3992399f60c62bf06ab8bddb8de69b3ea47
kul-sudo <ulyanov.kirill@protonmail.com>
2023-05-28 20:41:48 +0300

    test

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,32 +1,7 @@
-import { Server } from 'socket.io'
+import initializeSocketServer from '@/lib/server'
 
 export default (req, res) => {
-  if (res.socket.server.io) {
-    console.log('The socket has already been run.')
-    res.end()
-    return
-  }
-
-  const io = new Server(res.socket.server, {
-    path: '/api/socket',
-    addTrailingSlash: false
-  })
-
-  res.socket.server.io = io
-
-  io.on('connection', socket => {
-    socket.on('createRoom', roomUid => {
-      socket.join(roomUid)
-    })
-
-    socket.on('leaveRoom', roomUid => {
-      socket.leave(roomUid)
-    })
-
-    socket.on('sendMessage', data => {
-      io.to(data.uid).emit('messageReceived', { message: data.message, username: data.username })
-    })
-  })
-
+  initializeSocketServer(res.socket.server)
+  
   res.end()
 }
