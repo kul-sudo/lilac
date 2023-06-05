54b79adef923500f9cd369be9d5d563b89cea837
kul-sudo <ulyanov.kirill@protonmail.com>
2023-06-05 21:08:16 +0300

    feat: test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import { Button, HStack, Input, Text, VStack, useColorModeValue } from '@chakra-ui/react'
+import { BadgeCheckIcon } from 'lucide-react'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { BadgeCheckIcon } from 'lucide-react'
 import { addRoom, retrieveRooms } from '@/lib/firebaseOperations'
 import io from 'socket.io-client'
 
