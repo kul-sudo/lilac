8f9f937f8a65abcb6c31ea0ccca3e7296a1142c4
kul-sudo <ulyanov.kirill@protonmail.com>
2023-06-15 04:09:55 +0300

    feat: added server to api

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,7 +1,38 @@
-import initializeSocketServer from '@/lib/server'
+import { Server } from 'socket.io'
 
 export default (req, res) => {
-  initializeSocketServer(res.socket.server)
-  
+  if (res.socket.server.io) {
+    console.log('The socket has already been run.')
+    res.end()
+    return
+  }
+
+  const io = new Server(res.socket.server, {
+    path: '/api/socket',
+    addTrailingSlash: false
+  })
+
+  res.socket.server.io = io
+
+  io.on('connection', socket => {
+    // createRoom can both create and join a room
+    socket.on('createRoom', data => {
+      socket.join(data.uid)
+
+      io.to(data.uid).emit('userConnected', `${data.username} has joined the room`)
+    })
+
+    socket.on('leaveRoom', data => {
+      socket.leave(data.uid)
+
+      io.to(data.uid).emit('userLeft', `${data.username} has left the room`)
+    })
+
+    socket.on('sendMessage', data => {
+      socket.broadcast.to(data.uid).emit('messageReceived', { message: data.message, username: data.username, color: data.color, image: data.image })
+    })
+  })
+
+  console.log('Setting up the socket.')
   res.end()
 }
