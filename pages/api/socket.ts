978b0988260ab418d68d7f94b65623c2940c26c8
kul-sudo <ulyanov.kirill@protonmail.com>
2023-07-18 17:12:43 +0300

    feat: added leave message on tab close

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -2,6 +2,8 @@ import type { NextApiRequest } from 'next'
 import type { ClientToServerEvents, InterServerEvents, NextApiResponseWithSocket, ServerToClientEvents, SocketData } from '../../types/socket'
 import { Server } from 'socket.io'
 
+let socketsData = new Map()
+
 const socket = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
   if (res.socket.server.io) {
     res.status(404).send('The socket has already been run.')
@@ -22,16 +24,26 @@ const socket = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
       socket.join(data.uid)
 
       io.to(data.uid).emit('userConnected', `${data.username} has joined the room`)
+
+      socketsData.set(socket.id, { username: data.username, uid: data.uid })
     })
 
     socket.on('leaveRoom', data => {
       socket.leave(data.uid)
 
       io.to(data.uid).emit('userLeft', `${data.username} has left the room`)
+
+      socketsData.delete(socket.id)
     })
 
     socket.on('sendMessage', data => {
       socket.broadcast.to(data.uid).emit('messageReceived', { message: data.message, username: data.username, color: data.color, image: data.image })
+    })
+
+    socket.on('disconnect', () => {
+      const { username, uid } = socketsData.get(socket.id)
+      io.to(uid).emit('userLeft', `${username} has left the room`)
+      socketsData.delete(socket.id)
     })
   })
 
