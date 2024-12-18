f140427dadef9d722615e36895418ea2e58a7740
kul-sudo <ulyanov.kirill@protonmail.com>
2024-12-19 02:21:14 +0300

    fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app'
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: 'https://lilac-7d60a-default-rtdb.europe-west1.firebasedatabase.app',
+  databaseURL: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
